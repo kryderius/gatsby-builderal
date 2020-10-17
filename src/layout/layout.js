29b74bf4f151925c312c25eebb54b1d85f85eb5b
kryderius <krydegamer@gmail.com>
2020-10-17 17:09:57 +0200

    Demko 2

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -11,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Nunito Sans", sans-serif;
+    font-family: "Nunito Sans", normal;
   }
 `;
 
